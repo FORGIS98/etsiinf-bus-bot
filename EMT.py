71cd84d251130429329977ade7088c3738fb2995
FORGIS98 <cifradoforgis@gmail.com>
2019-12-09 00:39:51 +0100

    Now we get etsiinf and colonia bus times
    The implementation will change, I'm just working on it

--- EMT.py
+++ EMT.py
@@ -1,8 +1,5 @@
 import requests
-import shlex
 import json
-
-from subprocess import Popen, PIPE
 
 # open json files
 with open("buses.json") as f:
@@ -12,18 +9,10 @@ with open("buses.json") as f:
 URL = "http://api.interurbanos.welbits.com/v1/stop/"
 
 
-def getAllBusTimes(stopNumber):
+def getEtsiinf():
+    stopNumber = "08771"
     """Returns a dictionary with (busNumber => time) to stopNumber"""
     busLines =  data[stopNumber]["bus"]
-
-#    comando = "lwp-request " + URL + stopNumber
-#    print("----------------->", comando)
-#    args = shlex.split(comando)
-#    proc = Popen(args, stdout=PIPE)
-#    proc = Popen(args, stderr=PIPE)
-#
-#    output = proc.communicate()
-#    print("OUTPUT :: ", output)
 
     req = requests.get(URL + stopNumber)
     req = json.loads(req.content)
@@ -36,16 +25,60 @@ def getAllBusTimes(stopNumber):
             if(aux == busLines[j]):
                 answer[busLines[j]] = req["lines"][i]["waitTime"]
 
+    stopNumber = "17573"
+    busLines =  data[stopNumber]["bus"]
+
+    req = requests.get(URL + stopNumber)
+    req = json.loads(req.content)
+
+    for i in range (0, len(req["lines"])):
+        aux = req["lines"][i]["lineNumber"]
+        for j in range (0, len(busLines)):
+            if(aux == busLines[j]):
+                answer[busLines[j]] = req["lines"][i]["waitTime"]
+
+    stopNumber = "08411"
+    busLines =  data[stopNumber]["bus"]
+
+    req = requests.get(URL + stopNumber)
+    req = json.loads(req.content)
+
+    for i in range (0, len(req["lines"])):
+        aux = req["lines"][i]["lineNumber"]
+        for j in range (0, len(busLines)):
+            if(aux == busLines[j]):
+                answer[busLines[j]] = req["lines"][i]["waitTime"]
+
+    if(answer == {}):
+        return None
+    return answer
+
+def getColonia():
+    stopNumber = "08409"
+    """Returns a dictionary with (busNumber => time) to stopNumber"""
+    busLines =  data[stopNumber]["bus"]
+
+    req = requests.get(URL + stopNumber)
+    req = json.loads(req.content)
+
+    answer = {}
+
+    for i in range (0, len(req["lines"])):
+        aux = req["lines"][i]["lineNumber"]
+        for j in range (0, len(busLines)):
+            if(aux == busLines[j]):
+                answer[busLines[j]] = req["lines"][i]["waitTime"]
+
+    if(answer == {}):
+        return None
     return answer
 
 
-
-
-def main():
-    print("Here we go!")
-    res = getAllBusTimes("08771")
-
-    for i in res:
-        print(i, ":", res[i])
-
-main()
+#def main():
+#    print("Here we go!")
+#    res = getAllBusTimes("08771")
+#
+#    for i in res:
+#        print(i, ":", res[i])
+#
+#main()
