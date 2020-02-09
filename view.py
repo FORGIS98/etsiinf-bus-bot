4d6a47173aaf788827bca2b4fc74091715125f13
FORGIS98 <cifradoforgis@gmail.com>
2020-02-09 19:12:24 +0100

    new view, I should read more DOC

--- view.py
+++ view.py
@@ -1,84 +1,48 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler, CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import logging
 
 import os
-import controller as ctr
+import controller
 
-############################### Bot ############################################
-def start(bot, update):
-    update.message.reply_text(main_menu_message(), reply_markup=main_menu_keyboard())
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
-def main_menu(bot, update):
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def start(update, context):
+    keyboard = [[InlineKeyboardButton("Buses ETSIINF", callback_data='1'),
+                 InlineKeyboardButton("Buses Colinia", callback_data='2')]]
+                # [InlineKeyboardButton("Buses Moncloa", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Escoge una opción:', reply_markup=reply_markup)
+
+
+def button(update, context):
     query = update.callback_query
-    bot.edit_message_text(chat_id=query.message.chat_id,
-                          message_id=query.message.message_id,
-                          text=main_menu_message(),
-                          reply_markup=main_menu_keyboard())
 
-def paradaBus_menu(bot, update):
-    query = update.callback_query
-    bot.edit_message_text(chat_id=query.message.chat_id,
-                            message_id=query.message.message_id,
-                            text=paradaBus_menu_message(),
-                            reply_markup=paradaBus_menu_keyboard())
+    if(query.data == '1'):
+        query.edit_message_text(text=controller.etsiinf())
+    elif(query.data == '2'):
+        query.edit_message_text(text=controller.colonia())
 
-def etsiinfResponse(bot, update):
-    query = update.callback_query
-    bot.edit_message_text(chat_id=query.message.chat_id,
-                            message_id=query.message.message_id,
-                            text=second_menu_message())
-    etsiinfEMTcall()
 
-def first_submenu(bot, update):
-    pass
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-def second_submenu(bot, update):
-    pass
-
-############################ Keyboards #########################################
-def main_menu_keyboard():
-    keyboard = [[InlineKeyboardButton('Paradas Autobus', callback_data='paradaBus')]]
-    return InlineKeyboardMarkup(keyboard)
-
-def paradaBus_menu_keyboard():
-    keyboard = [[InlineKeyboardButton('ETSIINF', callback_data='etsiinfParada')],
-                [InlineKeyboardButton('Colonia Jardin', callback_data='coloniaJardinParada')],
-                [InlineKeyboardButton('Atras', callback_data='main')]]
-    return InlineKeyboardMarkup(keyboard)
-
-############################# Actions ##########################################
-
-def etsiinfEMTcall(bot, update):
-    text = ctr.etsiinf()
-    print("Ma text")
-    bot.send_message(chat_id=query.message.chat_id, text=text)
-
-############################# Messages #########################################
-
-def main_menu_message():
-    return '¿En que puedo ayudarte?'
-
-def paradaBus_menu_message():
-    return '¿Donde te encuentras?'
-
-def second_menu_message():
-    return 'Time for depart is:'
-
-############################# Handlers #########################################
 
 def main():
+    updater = Updater(token=os.environ["etsiinfBOT"], use_context=True)
 
-    updater = Updater(token=os.environ["etsiinfBOT"], use_context=False)
-    
     updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(paradaBus_menu, pattern='paradaBus'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(etsiinfResponse, pattern='etsiinfParada'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(first_submenu, pattern='coloniaJardinParada'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(second_submenu, pattern='m2_1'))
-    
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_error_handler(error)
+
     updater.start_polling()
-    # updater.idle()
+
+    updater.idle()
+
 
 main()
